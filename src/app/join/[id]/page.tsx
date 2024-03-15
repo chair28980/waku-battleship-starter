24916c6a7c3e1e501998088f94d4455c661b2e16
chair28980 <vrycmfy@protonmail.com>
2024-03-15 16:27:05 -0700

    add ContentPairProvider

--- src/app/join/[id]/page.tsx
+++ src/app/join/[id]/page.tsx
@@ -1,24 +1,28 @@
-'use client'
+"use client";
 import { useParams, useSearchParams } from "next/navigation";
 import React from "react";
 import Container from "../../components/Container";
 import { Player } from "../../types";
+import { ContentPairProvider } from "@waku/react";
 
 const Page = () => {
-    const searchParams = useParams();
-    const queryParams = useSearchParams();
-    const username = queryParams.get('username');
-    const roomId = searchParams.id as string;
+  const searchParams = useParams();
+  const queryParams = useSearchParams();
+  const username = queryParams.get("username");
+  const roomId = searchParams.id as string;
 
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
+  return (
+    <ContentPairProvider contentTopic={`/waku-battle-ship-tutorial/${roomId}`}>
+      <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
         <div className="text-lg font-bold text-center">
-            Welcome, <span className="text-green-500">{username}</span> <br />
-            you have joined the room <span className="text-blue-500"> {roomId} </span>
+          Welcome, <span className="text-green-500">{username}</span> <br />
+          you have joined the room{" "}
+          <span className="text-blue-500"> {roomId} </span>
         </div>
         <Container player={Player.p2} />
-        </div>
-    )
+      </div>
+    </ContentPairProvider>
+  );
 };
 
 export default Page;
--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,30 +1,33 @@
-'use client'
+"use client";
 
 import { useParams, useSearchParams } from "next/navigation";
 import React from "react";
 import Container from "../../components/Container";
 import { Player } from "../../types";
+import { ContentPairProvider } from "@waku/react";
 
 const Page = () => {
-    const searchParams = useParams();
-    const queryParams = useSearchParams();
-    const username = queryParams.get('username');
+  const searchParams = useParams();
+  const queryParams = useSearchParams();
+  const username = queryParams.get("username");
 
-    return (
-        <>
-            <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
-            <div className="text-lg font-bold text-center">
-                Welcome to room: <span className="text-blue-500">{searchParams.id}</span> created by <span className="text-green-500">{username}</span>
-            </div>
+  return (
+    <ContentPairProvider contentTopic={`/waku-battle-ship-tutorial/${roomId}`}>
+      <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
+        <div className="text-lg font-bold text-center">
+          Welcome to room:{" "}
+          <span className="text-blue-500">{searchParams.id}</span> created by{" "}
+          <span className="text-green-500">{username}</span>
+        </div>
 
-            <div className="text-md text-gray-700 text-center">
-                Share this room ID with your friend to start playing now
-            </div>
+        <div className="text-md text-gray-700 text-center">
+          Share this room ID with your friend to start playing now
+        </div>
 
-            <Container player={Player.p1} />
-            </div>
-        </>
-    )
-}
+        <Container player={Player.p1} />
+      </div>
+    </ContentPairProvider>
+  );
+};
 
 export default Page;
